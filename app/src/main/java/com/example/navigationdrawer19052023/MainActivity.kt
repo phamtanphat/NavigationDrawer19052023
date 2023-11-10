17b71781991a7e50fb6ba27535562593671c8a2a
phamtanphat <phatdroid94@gmail.com>
2023-11-10 18:24:58 +0700

    finish

--- app/src/main/java/com/example/navigationdrawer19052023/MainActivity.kt
+++ app/src/main/java/com/example/navigationdrawer19052023/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.navigationdrawer19052023
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -31,5 +34,22 @@ class MainActivity : AppCompatActivity() {
                 drawerLayout.open()
             }
         }
+
+        navigationView.setNavigationItemSelectedListener {
+            val fragment: Fragment = when (it.itemId) {
+                R.id.menu_item_gallery -> GalleryFragment()
+                R.id.menu_item_import -> ImportFragment()
+                R.id.menu_item_slideshow -> SlideShowFragment()
+                else -> ToolsFragment()
+            }
+            val fragmentTransaction = supportFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragment_container, fragment)
+            fragmentTransaction.commit()
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.close()
+            }
+            return@setNavigationItemSelectedListener true
+        }
     }
+
 }
