abf1d7c3cba1370527bb065657676e6f2162b115
phamtanphat <phatdroid94@gmail.com>
2023-11-10 17:57:40 +0700

    open drawer

--- app/src/main/java/com/example/navigationdrawer19052023/MainActivity.kt
+++ app/src/main/java/com/example/navigationdrawer19052023/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.navigationdrawer19052023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var toolBar: Toolbar
+    private lateinit var navigationView: NavigationView
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolBar = findViewById(R.id.toolbar)
+        navigationView = findViewById(R.id.navigation_view)
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        setSupportActionBar(toolBar)
+        toolBar.title = "Navigation drawer"
+        toolBar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size)
+        toolBar.setNavigationOnClickListener {
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.close()
+            } else {
+                drawerLayout.open()
+            }
+        }
     }
 }
